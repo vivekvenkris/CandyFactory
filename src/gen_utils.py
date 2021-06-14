d479239893ada724d20c450f55bd2b84bff83887
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2021-06-14 18:09:38 +0200

    bunch of general util functions

---
+++ src/gen_utils.py
@@ -0,0 +1,145 @@
+import sys, os, os.path, glob, subprocess, multiprocessing, shlex, shutil, copy
+from Log import logger
+
+
+def get_nearest_even_number(number_int):
+        return number_int if int(number_int) % 2 == 0 else number_int-1
+
+
+def ensure_correct_processing_status(status, reqd_status):
+        if(status < reqd_status):
+            IncorrectStatusException("Current processing status={} < required={}, aborting. ".format(status, reqd_status))
+
+        elif(status >= reqd_status):
+            IncorrectStatusException("Current processing status={} >  required={}, skipping. ".format(status, reqd_status) )
+
+
+
+       
+
+
+def run_process(command):
+
+        logger = Logger().logger
+
+        command_chunks = command.split()
+
+        datetime_start = (datetime.datetime.now()).strftime("%Y/%m/%d  %H:%M")
+        time_start = time.time()
+
+        logger.info("****************************************************************\n")
+        logger.info("START DATE AND TIME: %s\n" % (datetime_start))
+        logger.info("\nCOMMAND:\n")
+        logger.info("%s\n\n" % (command))
+        logger.info("WORKING DIRECTORY: %s\n" % (work_dir))
+        logger.info("****************************************************************\n")
+        log_file.flush()
+
+        proc = subprocess.Popen(command_chunks, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, envs = os.environ.copy())
+        proc.communicate()  #Wait for the process to complete                                                                                                                                                    
+
+        datetime_end = (datetime.datetime.now()).strftime("%Y/%m/%d  %H:%M")
+        time_end = time.time()
+
+        logger.info("\nEND DATE AND TIME: %s\n" % (datetime_end))
+        logger.info("\nTOTAL TIME TAKEN: %d s\n" % (time_end - time_start))
+
+
+def run_with_singularity(image, singularity_flags, command):
+
+
+        command = "singularity exec" + singularity_flags + " " + image + " " + command
+        run_process(command)
+
+
+
+from configparser import ConfigParser
+import os,sys
+import subprocess
+import shlex
+from subprocess import call, PIPE, STDOUT
+from errno import ENOENT
+
+def guess_type(data):
+    types = [int, float, complex, str]
+    for typename in types:
+        try:
+            val = typename(data)
+            if typename == str:
+                if data.lower() == "true":
+                    return True
+                elif data.lower() == "false":
+                    return False
+            return val
+        except:
+            pass
+
+
+def dir_exists(dir):
+    return os.path.exists(dir) and os.path.isdir(dir)
+
+
+def file_exists(file):
+    return os.path.exists(file) and os.path.isfile(file)
+
+
+def log_subprocess_output(pipe):
+    for line in iter(pipe.readline, b''): # b'\n'-separated lines
+        logging.info('got line from subprocess: %r', line)
+
+
+def run_subprocess(str_id, cmd_string, logger):
+    chunks = shlex.split(cmd_string)
+    p = subprocess.Popen(chunks, shell=True)
+    with p.stdout:
+        log_subprocess_output(p.stdout)
+    #exit_code = p.wait()
+
+def is_file_empty(file_name):
+
+    return os.stat(file_name).st_size == 0
+
+
+def ensure_file_exists(file_name):
+    if not file_exists(file_name):
+        raise IOError(ENOENT, 'No file found', file_name)
+
+
+def ensure_directory_exists(directory_name):
+    if not dir_exists(directory_name):
+        raise IOError(ENOENT, 'No directory found', directory_name)
+
+
+def ensure_directory_is_not_empty(directory_name):
+    if len(os.listdir(directory_name)) == 0:
+        raise IOError(ENOENT, 'No files in directory', directory_name)
+
+def backup_if_exists(source, logger):
+
+    if file_exists(source):
+        logger.warn(source + " exists.")
+        logger.warn("moving older file: " + source + " to " + source+".old")
+        os.rename(source, source+".old")
+
+def move_file(source, destination):
+    os.rename(source, destination)
+
+
+def get_path(dir, file):
+    os.path.join(dir, file)
+
+def write_array_pretty(file_name, array, logger):
+    
+    widths = [max(map(len, col)) for col in zip(*array)]
+
+    text_to_save= ""
+
+    for row in array:
+        text_to_save = text_to_save + "  ".join((val.ljust(width) for val, width in zip(row, widths))) + " \n"
+
+    logger.info("Writing to " +file_name)
+
+    with  open(file_name, 'w') as f:
+        f.write(text_to_save)
+
+
