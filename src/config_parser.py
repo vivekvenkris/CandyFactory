ac69ee5169270514b5556135f50dece8fc9eaca8
prajwalvp <prajwal3108@gmail.com>
2021-06-16 11:35:39 +0200

    Added changes to config file

--- src/config_parser.py
+++ src/config_parser.py
@@ -1,33 +1,19 @@
 import data_holders
+from  gen_utils import ensure_file_exists
 
 # Fixed definitions or paths  
 PULSARX_TEMPLATE = "/home/psr/software/PulsarX/include/template/meerkat_fold.template"
 
 
 class Configuration(object):
-    def __init__(self, config_filename):
-        self._config_filename = config_filename
-        self.dict_process_config={}
-
-        config_file = open( config_filename, "r" )
-
-        for line in config_file:
-            if line != "\n" and (not line.startswith("#")):
-            	key = line.split('#')[0].split(' ')[0]
-            	val = line.split('#')[0].split(' ')[1]
-                self.dict_process_config[key] = val      #Save parameter key and value in the dictionary 
-              
-        config_file.close()
-     
 
     def get_value_if_exists(flag, value):
         return flag +" "+ value if value.strip("\\s+") != "" else ""
 
 
-
     def generate_ddplan_flags():
 
-         return "-l {} -d {} -c {} -s {}".format(self.dict_process_config['DM_MIN'], self.dict_process_config['DM_MAX'], self.dict_process_config['DM_COHERENT_DEDISPERSION'], self.dict_process_config['N_SUBBANDS'])	
+         return "-l {} -d {} -c {} -s {}".format(self.dict_process_config['DM_MIN'], self.dict_process_config['DM_MAX'], self.dict_process_config['DM_COHERENT_DEDISPERSION'], self.dict_process_config['N_SUBBANDS'])  
 
      
     def generate_rfifind_flags():
@@ -48,76 +34,108 @@ class Configuration(object):
             raise Exception("You have specified zmax, wmax or ncpus which is already fixed.") 
 
 
-     	return  accelsearch_flags 
+        return  accelsearch_flags
+          
 
 
-     #cmd_rfifind = "rfifind %s -o %s -time %s -freqsig %s -timesig %s -intfrac %s -chanfrac %s %s %s %s" % (other_flags, infile_basename, time, freqsig, timesig, intfrac, chanfrac, flag_zapints, flag_zapchan, infile) 
-     #rfifind_extras =  
+    def __init__(self, config_filename):
+        self._config_filename = config_filename
+        self.dict_process_config={}
+
+        ensure_file_exists(config_filename)
+
+        config_file = open( config_filename, "r" )
+
+        for line in config_file:
+            if line != "\n" and (not line.startswith("#")):
+                key = line.split('#')[0].split(' ')[0]
+                val = line.split('#')[0].split(' ')[1]
+                self.dict_process_config[key] = val      #Save parameter key and value in the dictionary 
+              
+        config_file.close()
      
+        
+        singularity_flags = "-B {}:{} -B {}:{}".format(self.dict_process_config['PROCESSING_PATH'], 
+                                                    self.dict_process_config['PROCESSING_PATH'],
+                                                    self.dict_process_config['OUTPUT_PATH'],
+                                                    self.dict_process_config['OUTPUT_PATH'])
 
-    # All necessary flags 
-
-    singularity_flags = "-B {}:{} -B {}:{}".format(self.dict_process_config['PROCESSING_PATH'], 
-     	                                            self.dict_process_config['PROCESSING_PATH'],
-     	                                            self.dict_process_config['OUTPUT_PATH'],
-     	                                            self.dict_process_config['OUTPUT_PATH'])
-
-    pulsarX_flags = " -L {} "
+        pulsarX_flags = " -L {} -n {} -b {}".format(self.dict_process_config['NSUBINT_FOLD'],
+                                                self.dict_process_config['NCHAN_FOLD']),
+                                                self.dict_process_config['NBIN_FOLD'])
 
 
-    
+        all_segment_configs = []
 
-    observations =  Observation(self.dict_process_config['SOURCE'],
+
+        get_acc_range_and_segment_fraction(self.dict_process_config['ACC_SEGMENT_LIST'])
+
+
+        observations =  Observation(self.dict_process_config['SOURCE'],
                                  self.dict_process_config['NBEAMS'], 
                                  self.dict_process_config['TOBS'], 
                                  self.dict_process_config['BAND'], 
                                  self.dict_process_config['OBS_NO']) 
    
 
-    presto_config = PrestoConfig(self.dict_process_config['PRESTO_IMAGE'], 
+        presto_config = PrestoConfig(self.dict_process_config['PRESTO_IMAGE'], 
                                   singularity_flags  
-     	                          rfifind_flags,
-     	                          ddplan_flags,
-     	                          accelsearch_flags)  
+                                  rfifind_flags,
+                                  ddplan_flags,
+                                  accelsearch_flags)  
 
-    peasoup_config = PeasoupConfig(self.dict_process_config['PEASOUP_IMAGE'], 
-     	                            singularity_flags 
-      	                            self.dict_process_config['ACC_MIN'],
-     	                            self.dict_process_config['ACC_MAX'], 
-     	                            self.dict_process_config['LIST_SEGMENTS'],  
-                                    self.dict_process_config['START_OFFSET']
-                                    self.dict_process_config['SCALE_ACC_WITH_SEGMENTS'] 
-     	                            ) 
+        peasoup_config = PeasoupConfig(self.dict_process_config['PEASOUP_IMAGE'], 
+                                    singularity_flags,
+                                    all_segment_configs,                                    self.dict_process_config['START_OFFSET']
+                                    self.dict_process_config['END_OFFSET']
+                                    self.dict_process_config['DO_ZERO_ACC_BIRDIES'] 
+                                    self.dict_process_config['PEASOUP_FLAGS']
+                                    ) 
 
-    filelocations = FileLocations(self.dict_process_config['TAPE_PATH'],
+        filelocations = FileLocations(self.dict_process_config['TAPE_PATH'],
                                    self.dict_process_config['TAPE_MACHINE']  
-     	                           self.dict_process_config['STAGING_PATH'], 
-     	                           self.dict_process_config['STAGING_MACHINE'],
-     	                           self.dict_process_config['PROCESSING_PATH'] 
-     	                           ) 
+                                   self.dict_process_config['STAGING_PATH'], 
+                                   self.dict_process_config['STAGING_MACHINE'],
+                                   self.dict_process_config['PROCESSING_PATH'] 
+                                   ) 
 
 
-    pulsarX_config =  PulsarXConfig(self.dict_process_config['PULSARX_IMAGE'],
-     	                             singularity_flags,
-     	                             self.dict_process_config['PULSARX_ZERODM_MATCHED_FILTER'],
-     	                             self.dict_process_config['NSUBINT_FOLD'], 
+        pulsarX_config =  PulsarXConfig( self.dict_process_config['PULSARX_IMAGE'],
+                                     singularity_flags,
+                                     self.dict_process_config['PULSARX_ZERODM_MATCHED_FILTER'],
+                                     self.dict_process_config['NSUBINT_FOLD'], 
                                      self.dict_process_config['NBIN_FOLD'],
-                                     self.dict_process_config[])
+                                     self.dict_process_config['NCHAN_FOLD']),
+                                     self.dict_process_config['ADDITIONAL_PULSARX_FLAGS'])
+
+
+    
+
+        slurm_config =SlurmConfig(self.dict_process_config['N_SIMULTANEOUS_JOBS'],
+                                  self.dict_process_config['PARTITION'],
+                                  self.dict_process_config['MAIL_USER'],
+                                  self.dict_process_config['MAIL_TYPE']) 
+
+
+        # Assign segment_fractions from lengths 
+
+
+        self.dict_process_config['LIST_SEGMENTS']
+
+        # Convert acc segment list to segment config object
+
+        configs = []
+
+        segments_config = SegmentConfig(self.dict_process_config['LIST_SEGMENTS'],
+                                        self.dict_process_config['ACC_MIN'],
+                                        self.dict_process_config['ACC_MAX'])                              
+
+        config =  Config(filelocations, presto_config, peasoup_config, pulsarX_config, slurm_config, self.dict_process_config['DM_FILE'])
+  
 
 
 
-
-    slurm_config =SlurmConfig(self.dict_process_config['N_SIMULTANEOUS_JOBS'],
-     	                       self.dict_process_conf
-     	                       ig['PARTITION'],
-     	                       self.dict_process_config['MAIL_USER'],
-     	                       self.dict_process_config['MAIL_TYPE'])    
-
-
-
-
-    config =  Config(filelocations, presto_config, peasoup_config, pulsarX_config, slurm_config, self.dict_process_config['DM_FILE'])
-
+ 
 
      
 
