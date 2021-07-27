6fa931cca87c9ed13295fb164cb8f312f32afc02
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2021-07-27 10:03:38 +0200

    fix argparse

--- src/excise_rfi.py
+++ src/excise_rfi.py
@@ -3,24 +3,24 @@ from rfi_utils import RFIUtils
 
 from constants import STATUS_DICT,StatusManager
 
-def get_args():
+def get_args(args):
     argparser = argparse.ArgumentParser(description="Excise RFI using PulsarX zero DM matched filter, and generate dynamic birdie list / zap mask for peasoup search")
     argparser.add_argument("-config", dest="config", help="configuration file")
     argparser.add_argument("-verbose", dest="verbose", help="Enable verbose terminal logging", action="store_true")
     argparser.add_argument("-beam_num", dest="beam_num", help="beam number to process")
     argparser.add_argument("-log_file_prefix", dest="log_file_prefix", help="Name of log file",default="candyfactory")
     argparser.add_argument("-ledger_file", dest="ledger_file", help="ledger_file")
-    args = argparser.parse_args()
+    args = argparser.parse_args(args)
     return args
 
 
-def main(args):
+def main(args=sys.argv[1:]):
 
-    args = get_args()    
+    args = get_args(args)    
     logger = init_logging(file_name="{}_{}.log".format(args.log_file_prefix,(datetime.datetime.now()).strftime("%Y-%m-%d-%H:%M:%S")),
                              file_level=logging.DEBUG if args.verbose else None)
     config = parse_config(args.config_file)
-    ledger = Ledger() is args.ledger_file is None else Ledger()
+    ledger = Ledger() if args.ledger_file is None else Ledger(args.ledger_file)
 
     logger.info("Using the following Ledger file: {}", ledger_file)
 
@@ -44,7 +44,7 @@ def main(args):
 
 
 if __name__ == __main__():
-    main(args)
+    main()
 
 
 
